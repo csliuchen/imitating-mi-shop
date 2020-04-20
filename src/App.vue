b9db0b69a0d293deb92a651a9511f91a5d3147a4
leongwenjun <leungwenjun@gmail.com>
2020-04-20 23:27:31 +0800

    终于开始做主页了之拖把

--- src/App.vue
+++ src/App.vue
@@ -11,25 +11,18 @@ export default {
   name: "App",
   components: {},
   mounted() {
-    //storage.setItem('userName',{A:1},'user')
-    //storage.setItem("score", { gpa: 3.2 }, "user");
-    //console.log(storage.getStorage()['user'])
-    //storage.clear('sex')
-    this.axios.get('/user/login').then((res)=>{
-      this.res = res;
-      //console.log(res);
-      });
+    // //storage.setItem('userName',{A:1},'user')
+    // //storage.setItem("score", { gpa: 3.2 }, "user");
+    // //console.log(storage.getStorage()['user'])
+    // //storage.clear('sex')
+    // this.axios.get('/user/login').then((res)=>{
+    //   this.res = res;
+    //   //console.log(res);
+    //   });
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/reset.scss'
 </style>
