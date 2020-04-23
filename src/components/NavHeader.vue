df9892825c246978dc0bf2898b9511f2947d0409
leongwenjun <leungwenjun@gmail.com>
2020-04-23 21:58:01 +0800

    提取mixin.scss（导航栏）

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -56,6 +56,7 @@ export default {
 </script>
 <style lang="scss">
 @import "./../assets/scss/base.scss";
+@import "./../assets/scss/mixin.scss";
 .header {
   .nav-topbar {
     height: 39px;
@@ -64,9 +65,7 @@ export default {
     color: #b0b0b0;
 
     .container {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
+      @include flex();
       a {
         display: inline-block;
         color: #b0b0b0; //a也有默认样式且比class高所以要覆盖样式
@@ -79,11 +78,8 @@ export default {
         text-align: center;
         color: #ffffff;
         .icon-cart {
-          display: inline-block;
-          width: 16px;
-          height: 12px;
-          background: url("/imgs/icon-cart-checked.png") no-repeat center; //填充与居中，注意这里的路径，很迷惑为什么不用加public
-          background-size: contain;
+          @include bgImg(16px, 12px, "/imgs/icon-cart-checked.png");
+          margin-right: 4px;
         }
       }
     }
@@ -107,20 +103,12 @@ export default {
           height: 55px;
           &:before {
             content: " "; //占位的，非常重要
-            display: inline-block;
-            width: 55px;
-            height: 55px;
-            background: url("/imgs/mi-logo.png") no-repeat center;
-            background-size: 55px;
+            @include bgImg(55px, 55px, "/imgs/mi-logo.png", 55px);
             transition: margin 0.2s;
           }
           &:after {
             content: " ";
-            display: inline-block;
-            width: 55px;
-            height: 55px;
-            background: url("/imgs/mi-home.png") no-repeat center;
-            background-size: 55px;
+            @include bgImg(55px, 55px, "/imgs/mi-home.png", 55px);
           }
           &:hover:before {
             margin-left: -55px;
@@ -162,11 +150,7 @@ export default {
             padding-left: 14px;
           }
           a {
-            display: inline-block;
-            width: 18px;
-            height: 18px;
-            background: url("/imgs/icon-search.png");
-            background-size: contain;
+            @include bgImg(18px, 18px, "/imgs/icon-search.png");
             margin-left: 17px;
           }
         }
