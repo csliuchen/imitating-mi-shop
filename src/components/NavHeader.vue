c6b855f05ad045057986128306274b396066587a
leongwenjun <leungwenjun@gmail.com>
2020-04-26 23:55:27 +0800

    navigation header completed

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -10,9 +10,10 @@
           <a href="javascript:;">协议规则</a>
         </div>
         <div class="topbar-user">
-          <a href="javascript:;">登陆</a>
-          <a href="javascript:;">注册</a>
-          <a href="javascript:;" class="my-cart">
+          <a href="javascript:;" v-if="username">{{username}}</a>
+          <a href="javascript:;" v-if="!username" @click="login">登陆</a>
+          <a href="javascript:;" v-if="username">{{username}}</a>
+          <a href="javascript:;" class="my-cart" @click="goToCart()">
             <span class="icon-cart"></span>购物车
           </a>
         </div>
@@ -26,15 +27,109 @@
         <div class="header-menu">
           <div class="item-menu">
             <span>小米手机</span>
-            <div class="children"></div>
+            <div class="children">
+              <ul>
+                <li class="product" v-for="(item,index) in phoneList" :key="index">
+                  <!-- a是行级元素，div是块级元素 -->
+                  <a v-bind:href="'/#/product/'+item.id" target="_blank">
+                    <div class="pro-img">
+                      <img :src="item.mainImage" :alt="item.subtitle" />
+                    </div>
+                    <div class="pro-name">{{item.name}}</div>
+                    <div class="pro-price">{{item.price | currency}}</div>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div class="item-menu">
             <span>RedMi红米</span>
-            <div class="children"></div>
+            <!-- <div class="children"></div> -->
           </div>
           <div class="item-menu">
             <span>电视</span>
-            <div class="children"></div>
+            <div class="children">
+              <ul>
+                <li class="product">
+                  <!-- a是行级元素，div是块级元素 -->
+                  <a href target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/0112cb7e2ea8489fbd36ce3a781d5232.jpg"
+                        alt
+                      />
+                    </div>
+                    <div class="pro-name">Redmi 智能电视 MAX 98''</div>
+                    <div class="pro-price">19999元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <!-- a是行级元素，div是块级元素 -->
+                  <a href target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/6917267948c54c647d2009a98637aac6.jpg"
+                        alt
+                      />
+                    </div>
+                    <div class="pro-name">Redmi 红米电视 70英寸 R70A</div>
+                    <div class="pro-price">2999元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <!-- a是行级元素，div是块级元素 -->
+                  <a href target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/0112cb7e2ea8489fbd36ce3a781d5232.jpg"
+                        alt
+                      />
+                    </div>
+                    <div class="pro-name">小米壁画电视 65英寸</div>
+                    <div class="pro-price">6999元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <!-- a是行级元素，div是块级元素 -->
+                  <a href target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/0112cb7e2ea8489fbd36ce3a781d5232.jpg"
+                        alt
+                      />
+                    </div>
+                    <div class="pro-name">小米CC9</div>
+                    <div class="pro-price">1799元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <!-- a是行级元素，div是块级元素 -->
+                  <a href target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/0112cb7e2ea8489fbd36ce3a781d5232.jpg"
+                        alt
+                      />
+                    </div>
+                    <div class="pro-name">小米CC9</div>
+                    <div class="pro-price">1799元</div>
+                  </a>
+                </li>
+                <li class="product">
+                  <!-- a是行级元素，div是块级元素 -->
+                  <a href target="_blank">
+                    <div class="pro-img">
+                      <img
+                        src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/0112cb7e2ea8489fbd36ce3a781d5232.jpg"
+                        alt
+                      />
+                    </div>
+                    <div class="pro-name">小米CC9</div>
+                    <div class="pro-price">1799元</div>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <!-- wrapper 是一个固定样式-->
           <div class="wrapper"></div>
@@ -51,12 +146,50 @@
 </template>
 <script>
 export default {
-  name: "nav-header"
+  name: "nav-header",
+  data() {
+    return {
+      username: "",
+      phoneList: []
+    };
+  },
+  filters: {
+    currency(val) {
+      if (!val) return "0.00";
+      return "¥" + val.toFixed(2) + "元";
+    }
+  },
+  mounted() {
+    this.getProductList();
+  },
+  methods: {
+    login(){
+      this.$router.push('/login')
+    },
+    getProductList() {
+      this.axios
+        .get("/products", {
+          //get post 穿参是有区别的
+          params: {
+            categoryId: "100012"
+          }
+        })
+        .then(res => {
+          if (res.list.length > 6) {
+            this.phoneList = res.list.slice(0, 6);
+          }
+        });
+    },
+    goToCart(){
+      this.$router.push('/cart')
+    }
+  }
 };
 </script>
 <style lang="scss">
 @import "./../assets/scss/base.scss";
 @import "./../assets/scss/mixin.scss";
+@import "./../assets/scss/config.scss";
 .header {
   .nav-topbar {
     height: 39px;
@@ -86,6 +219,7 @@ export default {
   }
   .nav-header {
     .container {
+      position: relative;
       height: 112px;
       display: flex;
       justify-content: space-between;
@@ -130,7 +264,68 @@ export default {
           span {
             cursor: pointer;
           }
-          & :hover {
+          &:hover {
+            color: $colorA;
+            .children {
+              height: 220px;
+              opacity: 1;
+            }
+          }
+          .children {
+            position: absolute; //绝对定位
+            top: 112px;
+            left: 0;
+            width: 1226px; //安全区域
+            height: 0px;
+            opacity: 0; //透明
+            overflow: hidden;
+            border-top: 1px solid #e5e5e5;
+            box-shadow: 0px 7px 6px 0px rgba(0, 0, 0, 0.11);
+            z-index: 10;
+            //background-color: #ffffff;
+            //防止因为层级关系被遮住
+            transition: all 0.5s;
+            .product {
+              position: relative;
+              float: left;
+              width: 16.6%;
+              height: 220px;
+              font-size: 12px;
+              line-height: 12px;
+              text-align: center;
+              a {
+                display: inline; //一个让检查元素时候更方便的小技巧
+              }
+              img {
+                height: 111px;
+                width: auto;
+                margin-top: 26px;
+              }
+              .pro-img {
+                height: 137px;
+              }
+              .pro-name {
+                font-weight: bold;
+                margin-top: 19px;
+                margin-bottom: 8px;
+                color: $colorB;
+              }
+              .pro-price {
+                color: $colorA;
+              }
+              &:before {
+                content: " ";
+                position: absolute;
+                top: 28px;
+                right: 0;
+                border-left: 1px solid $colorF;
+                height: 100px;
+                width: 1px;
+              }
+              &:last-child:before {
+                display: none;
+              }
+            }
           }
         }
       }
