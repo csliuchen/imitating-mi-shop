990b04b96088071bc743669ad1df7817d732577f
leongwenjun <leungwenjun@gmail.com>
2020-05-01 11:28:24 +0800

    展开列表

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,7 +6,17 @@
           <ul class="menu-wrap">
             <li class="menu-item">
               <a href="javascript:;">手机 电话卡</a>
-              <div class="children"></div>
+              <div class="children">
+                <ul v-for="(item,i) in menuList" v-bind:key="i">
+                  <li v-for="(sub,j) in item" v-bind:key="j">
+                    <!-- 此处语法要留意，要理解通透 -->
+                    <a v-bind:href="sub?'/#/product/'+sub.id:''">
+                      <img v-bind:src="sub?sub.img:'/imgs/item-box-1.png'" alt />
+                      {{sub?sub.name:"小米9"}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">电视 盒子</a>
@@ -103,44 +113,108 @@ export default {
           id: "",
           img: "/imgs/slider/slide-1.jpg"
         }
+      ],
+      menuList: [
+        [
+          {
+            id: 30,
+            img: "/imgs/item-box-1.png",
+            name: "小米CC9"
+          },
+          {
+            id: 31,
+            img: "/imgs/item-box-2.png",
+            name: "小米8青春版"
+          },
+          {
+            id: 32,
+            img: "/imgs/item-box-3.jpg",
+            name: "Redmi K20 Pro"
+          },
+          {
+            id: 33,
+            img: "/imgs/item-box-4.jpg",
+            name: "移动4G专区"
+          }
+        ],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
       ]
     };
   }
 };
 </script>
 <style lang='scss'>
-@import './../assets/scss/mixin.scss';
-@import './../assets/scss/config.scss';
+@import "./../assets/scss/mixin.scss";
+@import "./../assets/scss/config.scss";
 .index {
   .swiper-box {
-    .nav-menu{
-      position:absolute;
-      width:264px;
+    .nav-menu {
+      position: absolute;
+      width: 264px;
       height: 451px;
       z-index: 8;
-      padding: 26px 0;//此处了解一下盒模型
-      background-color: #55585A7A;
-      box-sizing: border-box;//把padding也算进去
-      .menu-wrap{
-        .menu-item{
+      padding: 26px 0; //此处了解一下盒模型
+      background-color: #55585a7a;
+      box-sizing: border-box; //把padding也算进去
+      .menu-wrap {
+        .menu-item {
           height: 50px;
           line-height: 50px;
-          a{
-            position: relative;//用处是把箭头框起来
+          a {
+            position: relative; //用处是把箭头框起来
             display: block;
             font-size: 16px;
             color: white;
             padding-left: 30px;
-            &:after{//也了解一下before after
-            position: absolute;
-            right: 30px;
-            top: 17.5px;
-            content: ' ';
-              @include bgImg(10px,15px,'/imgs/icon-arrow.png');
+            &:after {
+              //也了解一下before after
+              position: absolute;
+              right: 30px;
+              top: 17.5px;
+              content: " ";
+              @include bgImg(10px, 15px, "/imgs/icon-arrow.png");
             }
           }
-          &:hover{
-            background-color:$colorA ;
+          &:hover {
+            background-color: $colorA;
+            .children {
+              display: block;
+            }
+          }
+          .children {
+            display: none;
+            width: 962px;
+            height: 451px;
+            background-color: $colorG;
+            position: absolute;
+            top: 0;
+            left: 264px; //此处需要留意
+            border: 1px solid $colorH;
+            ul {
+              display: flex;
+              justify-content: space-between; //两边对齐
+              height: 75px;
+              li {
+                height: 75px;
+                width: 241px;
+                line-height: 75px; //flex 1
+                padding-left: 23px;
+              }
+              a {
+                color: $colorB;
+                font-size: 14px;
+              }
+              img {
+                width: 42px;
+                height: 35px;
+                vertical-align: center;
+                margin-right: 15px;
+              }
+            }
           }
         }
       }
